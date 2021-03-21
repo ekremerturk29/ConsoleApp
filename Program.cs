fe8fc5e6911f3225e6df3f6d8915d1f30c24fe56
Ekrem Ertürk <58238771+ekremerturk29@users.noreply.github.com>
2021-03-21 22:05:05 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using ConsoleApp.Models;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new User() {username="ekremerturk" ,password="E12345e12345"};
+
+            var json = JsonConvert.SerializeObject(person);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:8834/session";
+            var handler = new HttpClientHandler()
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+            using var client = new HttpClient(handler);
+            var response = await  client.PostAsync(url, data);
+           
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
